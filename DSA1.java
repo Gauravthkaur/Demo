ce6e0c747ffc07f97d445a79e0830bc6a4de7779
Gauravthkaur <gaurav079t@gmail.com>
2025-04-11 17:05:37 +0530

    initial commit

---
+++ DSA1.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String args[]) {
+        int Marks = 40;
+        if ((Marks >= 35 ) && ( Marks <= 75)) {
+            System.out.println("PAss");
+        } else if (Marks > 75) {
+            System.out.println("honers");
+        } else {
+            System.out.println("Fails");
+        }
+    }
+}
